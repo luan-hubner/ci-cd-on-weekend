ae709becf9601ae78e1efb1642c583a336b5c87f
Luan Hubner <luanhubner.pro@gmail.com>
2026-01-10 14:29:59 -0400

    test: setup jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const { resolve } = require('path');
+const root = resolve(__dirname);
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: [
+    '<rootDir>/src/**/*.spec.ts',
+    '<rootDir>/test/**/*.spec.ts'
+  ],
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+};
